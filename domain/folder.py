90a4e06a0a72a946f69fb7be15e19b8205cf3fd2
olav <olav@olav-P5KC.(none)>
2013-02-13 13:35:25 +0100

    added debug logging for unicode errors

--- domain/folder.py
+++ domain/folder.py
@@ -90,6 +90,14 @@ class Folder(Node):
             files = [o for o in os.listdir(
                 self.sys_path) if (os.path.isfile(self.sys_path+'/'+o) and not
                 os.path.islink(self.sys_path+'/'+o))]
+        except UnicodeDecodeError, e:
+            app.logger.error(str(e))
+            app.logger.error(type(self.sys_path))
+            app.logger.error(self.sys_path)
+            self.sys_path = self.sys_path.decode('utf-8')
+            files = [o for o in os.listdir(
+                self.sys_path) if (os.path.isfile(self.sys_path+'/'+o) and not
+                os.path.islink(self.sys_path+'/'+o))]
         for line in files:
             path = u'{folder}/{file}'.format(folder=self.path, file=line)
             node = Node.get_instance(path)
